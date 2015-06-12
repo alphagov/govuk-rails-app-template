e87de9697594f7f965e7dc0827499746d9cb7358
Tekin Suleyman <tekin@tekin.co.uk>
2015-06-12 15:21:04 +0100

    Read logstash initializer from a file

--- template.rb
+++ template.rb
@@ -13,7 +13,7 @@ template 'templates/README.md.erb', 'README.md'
 template 'templates/LICENSE.erb', 'LICENSE'
 
 # Enable JSON-formatted logging in production
-application nil, env: "production" do <<-'RUBY'
+environment nil, env: "production" do <<-'RUBY'
 config.logstasher.enabled = true
   config.logstasher.logger = Logger.new(Rails.root.join("/log/production.json.log"))
   config.logstasher.suppress_app_log = true
@@ -23,14 +23,4 @@ end
 gsub_file 'config/environments/production.rb', 'config.log_formatter = ::Logger::Formatter.new', '# config.log_formatter = ::Logger::Formatter.new'
 
 # Configure JSON-formatted logging with additional fields
-initializer "logstasher.rb" do <<-'RUBY'
-if Object.const_defined?('LogStasher') && LogStasher.enabled
-  LogStasher.add_custom_fields do |fields|
-    # Mirrors Nginx request logging, e.g GET /path/here HTTP/1.1
-    fields[:request] = "\#{request.request_method} \#{request.fullpath} \#{request.headers['SERVER_PROTOCOL']}"
-    # Pass request Id to logging
-    fields[:govuk_request_id] = request.headers['GOVUK-Request-Id']
-  end
-end
-RUBY
-end
+initializer "logstasher.rb", File.read("#{File.dirname(__FILE__)}/templates/initializers/logstasher.rb")
